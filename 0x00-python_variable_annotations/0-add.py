8171a20b15e2794b700d279848357fd3c73aa779
michaeljumah <michaeljumah8@gmail.com>
2024-03-14 14:23:46 -0700

    mandatory tasks

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Basic annotations - add """
+
+def add(a: float, b: flaot) -> float:
+    """ Type-annotated function add that takes a float argument.
+        Args:
+         a: float type.
+          b: float type
+        Return:
+            sum as a float type
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+""" takes a string str1 and a string str2 as arguments and returns a concatenated string"""
+
+def concat(str1: str, str2: str) -> str:
+    """ Type-annotated function concat that takes a string arguments
+        Args:
+            str1: str type.
+            str2: str type.
+        Return:
+            concatinated string
+    """
+    return str1 + str2
+
