f605adc259859b643c00dc9e46b242987816db95
michaeljumah <michaeljumah8@gmail.com>
2024-03-14 14:46:16 -0700

    1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,12 +4,11 @@
 
 
 def concat(str1: str, str2: str) -> str:
-    """ Type-annotated function concat that takes a string arguments
+    """ Type-annotated function concat that takes a string arguments.
         Args:
             str1: str type.
             str2: str type.
         Return:
-            concatinated string
+            concatinated string.
     """
     return str1 + str2
-
